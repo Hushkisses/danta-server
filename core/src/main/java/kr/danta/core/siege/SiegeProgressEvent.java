83367883b2bbc8236e42965eb17cf75faf49f8b3
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-16 22:24:29 +0900

    DEV-113 add siege progression events

---
+++ core/src/main/java/kr/danta/core/siege/SiegeProgressEvent.java
@@ -0,0 +1,8 @@
+package kr.danta.core.siege;
+
+/** DEV-113 events that advance an engagement profile. */
+public enum SiegeProgressEvent {
+    QUICK_RESOLVED,
+    BATTLE_WON,
+    GATE_BREACHED
+}
